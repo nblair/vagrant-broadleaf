4017fedd0a232c2d1ec759edcfa9deaa8a701566
Paul Erickson <paul.erickson@wisc.edu>
2015-03-26 01:33:36 +0100

    Use a rough init script to run the demo site as a service at startup

--- manifests/base.pp
+++ manifests/base.pp
@@ -14,7 +14,7 @@ Package {
 }
 package { 'maven':
   install_options => ['--no-install-recommends'],
-  before          => Package['oracle-java7-installer'],
+  require => [Package['oracle-java7-installer'], Exec['accept-java-license']]
 }
 package { "ant":
   before => Package['oracle-java7-installer'],
@@ -43,6 +43,9 @@ exec { 'accept-java-license':
 package { 'oracle-java7-installer':
   ensure => present,
 }
+file { '/etc/environment':
+  content => inline_template("JAVA_HOME=/usr/lib/jvm/java-7-oracle")
+}
     
 exec { 'download broadleaf DemoSite':
   command => "/bin/sh -c 'wget http://downloads.broadleafcommerce.org/DemoSite-${DemoSite_version}-eclipse-workspace.zip'",
@@ -59,27 +62,26 @@ exec { 'unzip DemoSite':
 } 
 exec { 'mvn install':
   command => "mvn -X install",
-  require => [Package['maven'], Exec['unzip DemoSite']],
+  require => [Package['maven'], Exec['unzip DemoSite'], File['/etc/environment']],
   cwd => "/vagrant/eclipse-workspace/DemoSite",
   logoutput => true,
   timeout => 1200,
   creates => "/vagrant/eclipse-workspace/DemoSite/site/target/mycompany.war",
 }
 
-# TODO: this is broken. it's valid form, but we get the following error from puppet during provisioning:
-# ==> default: Info: /Service[jetty-site]: Unscheduling refresh on Service[jetty-site]
-# and the service isn't started. We probably will need to write our own init script.
-# it would also be nice to get log output from said init script.
-#service { 'jetty-site':
-#  provider => 'base',
-#  ensure => running,
-#  enable => true,
-#  start => 'cd /vagrant/eclipse-workspace/DemoSite/site; ant jetty-demo',
-#  stop => 'cd /vagrant/eclipse-workspace/DemoSite/site; ant jetty-stop',
-#  hasstatus => false,
-#  pattern => 'address=8000',
-#  require => Exec['mvn install'],
-#}
+file { '/etc/init.d/broadleaf-demo':
+  source => "/vagrant/broadleaf-demo.initscript",
+  mode => "0755"
+}
+
+service { 'broadleaf-demo':
+  ensure => running,
+  enable => true,
+  hasstatus => false,
+  require => [File['/etc/init.d/broadleaf-demo'], Package['maven'], Exec['mvn install']]
+}
+
+#TODO: make a similar init script for the admin project
 #service { 'jetty-admin':
 #  provider => 'base',
 #  ensure => running,
@@ -89,4 +91,26 @@ exec { 'mvn install':
 #  hasstatus => false,
 #  pattern => 'address=8001',
 #  require => Service['jetty-site'],
+#}
+
+#TODO: consider setting up jetty or tomcat as a service
+#package { 'jetty8':
+#  ensure => installed
+#}
+#exec { 'copy war':
+#  command => "/bin/ln -s /vagrant/eclipse-workspace/DemoSite/site/target/mycompany /usr/share/jetty8/webapps/",
+#  require => [Package['jetty8'], Exec['mvn install']],
+#  creates => "/usr/share/jetty8/webapps/mycompany",
+#}
+#exec { 'chown war':
+#  command => "/bin/chown jetty:adm /usr/share/jetty8/webapps/mycompany",
+#  require => [Exec['copy war']],
+#}
+#exec { 'autostart jetty':
+#  command => "/bin/sed -i 's/NO_START=1/NO_START=0/' /etc/default/jetty8",
+#  require => [Package['jetty8']],
+#}
+#exec { 'jvm args':
+#  command => "/bin/sed -i 's|#JAVA_OPTIONS.*$|JAVA_OPTIONS=\"-XX:MaxPermSize=256M -Xmx512M -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n -javaagent:/vagrant/eclipse-workspace/DemoSite/lib/spring-instrument-3.2.12.RELEASE.jar -Druntime.environment=development\"|' /etc/default/jetty8",
+#  require => [Package['jetty8']],
 #}
